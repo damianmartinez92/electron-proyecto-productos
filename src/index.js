e5c92f24ac5104c5cb6de3e2598cf50b0f7f8af8
dmartinez <damian.martinez@live.com.ar>
2019-07-19 12:13:20 -0300

    Proyecto Electron terminado, se puede agregar, eliminar uno o todos los productos. Preparado para produccion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // app inicia Electon, BrowserWindow crea la pantalla principal, Menu son las opciones del programa
-const {app, BrowserWindow, Menu} = require('electron')
+const {app, BrowserWindow, Menu, ipcMain} = require('electron')
 
 const path = require('path')
 const url = require('url')
@@ -11,14 +11,19 @@ if(process.env.NODE_ENV !== 'production'){
     })
 }
 
-
-
+// Pantalla principal
 let mainWindow
+// Pantalla de crear producto
 let newProductWindow
 
 // Esto se inicia al estar 'ready', con el path busca la carpeta src y ejecuta el index.html como pantalla principal
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({})
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true
+        }
+    })
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'views/index.html'),
         protocol: 'file',
@@ -26,20 +31,24 @@ app.on('ready', () => {
     }))
     // al cerrar el mainWindow cierra todas las pantallas abiertas
     mainWindow.on('closed', () => {
-        app.quit();
+        app.quit()
     })
-
     // Opciones del menu superior del programa, se le pasa la const templateMenu para ser mas prolijo y con el set se le asigna el nuevo Menu
     const mainMenu = Menu.buildFromTemplate(templateMenu)
     Menu.setApplicationMenu(mainMenu)
 
 })
 
+// Funcion para abrir pantalla de crear producto
 function createANewProductWindow(){
     newProductWindow = new BrowserWindow({
         width: 400,
         height: 330,
-        title: "Add A New Product"
+        title: "Add A New Product",
+        webPreferences: {
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true
+        }
     })
     newProductWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'views/new-product.html'),
@@ -49,6 +58,15 @@ function createANewProductWindow(){
     // elimina el menu superior en la ventana abierta
     newProductWindow.setMenu(null)
 }
+
+// Obtenemos los datos de new-product.html y los enviamos al index.html
+ipcMain.on('product:new', (e, newProduct) => {
+    // console.log(newProduct) podemos chequear que los valores son los correctos
+
+    // Se envía por mainWindow con webContents los datos obtenidos y luego cerramos la pantalla newProductWindow 
+    mainWindow.webContents.send('product:new', newProduct);
+    newProductWindow.close();
+})
 
 const templateMenu = [
     {
@@ -63,6 +81,9 @@ const templateMenu = [
             },
             {
                 label: "Remove All Product's",
+                click(){
+                    mainWindow.webContents.send('products:remove-all')
+                }
             },
             {
                 label: "Exit",
@@ -85,6 +106,7 @@ if(process.env.NODE_ENV !== 'production'){
         submenu: [
             {
                 label: "Show/Hide Dev Tools",
+                accelerator: process.platform === 'darwin' ? "cmd+D" : "Ctrl+D",
                 click(item, focusedWindow){
                     focusedWindow.toggleDevTools()
                 }
